85252ad0f53ab2009c7acdc874d2b47d8d07e68a
aasthathakur534 <aasthakur1947@gmail.com>
2025-10-30 08:34:51 +0530

    feat: add pages

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+
+
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -10,16 +15,24 @@ const Navbar = () => {
         <p>Shopie</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => setMenu("shop")}>
+          <Link to="/">Shop</Link>{menu === "shop" ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link to="/mens">Men</Link>{menu === "mens" ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("women")}>
+          <Link to="/womens">Women</Link>{menu === "women" ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link to="/kids">Kids</Link>{menu === "kids" ? <hr /> : null}
+        </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src = {cart_icon} alt = ""></img>
+        <Link to = '/login'><button>Login</button></Link>
+        <Link to = '/cart'> <img src={cart_icon} alt="" /></Link>
+        <div className="nav-cart-count">0</div>
       </div>
-      
     </div>
   )
 }
