e8ee6be3f17537ca19240fd1721ab7e7d61d8281
aasthathakur534 <aasthakur1947@gmail.com>
2025-10-31 16:31:23 +0530

    feat: add item and popular components also import these at  shop page

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -5,6 +5,15 @@ export const Item = (props) => {
     <div className='item'>
         <img src={props.image} alt="" />
         <p>{props.name}</p>
+        <div className="item-prices">
+            <div className="item-price-new">
+                {props.new_price}
+            </div>
+            <div className = "item-price-old">
+                {props.old_price}
+            </div>
+        </div>
     </div>
   )
 }
+export default Item;
