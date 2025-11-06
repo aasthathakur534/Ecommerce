09e1802fad1a45193c7ace64806b89bd1eb4d694
aasthathakur534 <aasthakur1947@gmail.com>
2025-11-06 23:24:37 +0530

    feat: add Description of products

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className='descriptionbox-nav-box'>Description</div>
+            <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p> 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quam sed consequatur aliquam perferendis ipsum in laboriosam consectetur iure nemo, commodi aliquid minima officiis illum nihil autem, nisi asperiores velit?
+            </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
